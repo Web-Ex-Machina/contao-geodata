34a1bd901bd427d9b4b69499c12555b1a53904c6
Quentin Vansteene <vansteene.quentin@gmail.com>
2023-01-30 19:24:49 +0100

    [feat][leaflet] enable gestureHandling and fitBounds parameter add double click event on map (retracting panels) add callback for applyFilters function code clean up

--- src/Resources/public/js/leaflet.js
+++ src/Resources/public/js/leaflet.js
@@ -1,55 +1,3 @@
-// // Make a custom leaflet marker to add important property
-// var LocationMarker = L.Marker.extend({
-// 	options: {
-// 		locationID : 1
-// 	},
-// });
-
-// function initMap() {
-// 	objMapData.forEach(function(location,index){
-// 		if(''!=location.lat && ''!=location.lng)
-// 			objMarkers[location.id].latLng = L.latLng({lat: parseFloat(location.lat), lng: parseFloat(location.lng)});
-// 		else
-// 			objMarkers[location.id].latLng = L.latLng({lat: 0, lng: 0});
-// 	});
-// 	objMap = map.remove();
-// 	objMap = L.map($map[0]);
-// 	objMapBounds = L.latLngBounds();
-// 	var options = {};
-
-// 	for(var i in objMarkers){
-// 		objMapBounds.extend(objMarkers[i].latLng);
-// 		options = {};
-// 		options.title = objMarkers[i].title;
-// 		options.locationID = objMarkers[i].id;
-
-// 		console.log(objMarkers[i]);
-
-// 		if(objMarkers[i].category.marker && objMarkers[i].category.marker.icon)
-// 			options.icon = L.icon(objMarkers[i].category.marker.icon);
-
-// 		objMarkers[i].marker = new LocationMarker(objMarkers[i].latLng, options).addTo(objMap);
-
-// 		if(0 < $('.map__list').length){
-// 			objMarkers[i].marker.on('click', function(e) {
-// 				selectMapItem(this.options.locationID);
-// 			});
-// 		}
-// 	}
-
-// 	objMap.setView(objMapBounds.getCenter(), objMapConfig.map.zoom);
-// 	L.tileLayer(objMapConfig.tileLayer.url, objMapConfig.tileLayer).addTo(objMap);
-
-// 	objMap.fitBounds(objMapBounds);
-// 	objMap.zoomControl.setPosition('bottomleft');
-
-
-// 	$toggleList.bind('click', function(){
-// 		objMap.invalidateSize();
-// 	});
-// }
-
-
 // LEAFLET
 var markersCluster    = new L.MarkerClusterGroup({});
 var objMarkersBounds  = L.latLngBounds();
@@ -59,6 +7,8 @@ var mapDefaultConfig  = {
 	maxZoom              : 13,
 	zoomControl          : true,
 	zoomControlPosition  : 'bottomleft',
+	gestureHandling  	 : true,
+	fitBounds  	 		 : true,
 	mapUrl               : 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
 	mapAttribution       : '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
 	marker               :  
@@ -69,37 +19,14 @@ var mapDefaultConfig  = {
 };
 
 
-selectMapItem = function(itemID){
-	$list.removeClass('has-selected');
-	$map.find('.leaflet-marker-pane').removeClass('has-selected');
-	$list.find('.map__list__item').removeClass('selected');
-	$map.find('.leaflet-marker-pane .leaflet-marker-icon').removeClass('selected');
-	if (itemID) {
-		$list.addClass('has-selected');
-		$list.find('.map__list__item[data-id="'+itemID+'"]').addClass('selected').get(0).scrollIntoView({behavior: "smooth", block: 'center', inline: "nearest"});
-		var marker = arrMarkersAll.filter(function(marker){
-		    return marker.locationID == itemID
-		});
-		if (marker[0]._icon) {
-			$map.find('.leaflet-marker-pane').addClass('has-selected');
-			$(marker[0]._icon).addClass('selected')
-		} else{
-		 	markersCluster.zoomToShowLayer(marker[0],()=>{selectMapItem(itemID)})
-		}
-	}
-}
-
-markersCluster.on('clusterclick', function (a) {
-	selectMapItem(false);
-});
-
-
-function initMap() {
+initMap = function() {
 	return new Promise(function(resolve,reject){
 		if (objMapConfig.map                	 === undefined ) objMapConfig.map               	  = {};
 		if (objMapConfig.map.zoom                === undefined ) objMapConfig.map.zoom                = mapDefaultConfig.zoom;
 		if (objMapConfig.map.zoomControl         === undefined ) objMapConfig.map.zoomControl         = mapDefaultConfig.zoomControl;
 		if (objMapConfig.map.zoomControlPosition === undefined ) objMapConfig.map.zoomControlPosition = mapDefaultConfig.zoomControlPosition;
+		if (objMapConfig.map.gestureHandling     === undefined ) objMapConfig.map.gestureHandling     = mapDefaultConfig.gestureHandling;
+		if (objMapConfig.map.fitBounds           === undefined ) objMapConfig.map.fitBounds           = mapDefaultConfig.fitBounds;
 
 		if (objMapConfig.tileLayer               === undefined) objMapConfig.tileLayer                = {};
 		if (objMapConfig.tileLayer.url           === undefined) objMapConfig.tileLayer.url            = mapDefaultConfig.mapUrl;
@@ -150,12 +77,12 @@ function initMap() {
 		// console.log(objMapConfig);
 		// console.log(objMarkersConfig);
 		// console.log(mapDefaultConfig);
-		var zoomControl = objMapConfig.map.zoomControl || mapDefaultConfig.zoomControl;
 	    map = L.map('map',{
-			zoomControl : zoomControl,
+			zoomControl : objMapConfig.map.zoomControl,
+			gestureHandling: objMapConfig.map.gestureHandling,
 	    }).setView([48.833, 2.333], objMapConfig.map.zoom); // LIGNE 18
 		map.attributionControl.setPosition('bottomleft');
-		if (zoomControl)
+		if (objMapConfig.map.zoomControl)
 			map.zoomControl.setPosition(mapDefaultConfig.zoomControlPosition);
 
 	    var layer = L.tileLayer(objMapConfig.tileLayer.url,
@@ -176,6 +103,12 @@ function initMap() {
 		map.setMaxBounds(bounds);
 		map.on('drag', function() {map.panInsideBounds(bounds, { animate: false }); });
 		map.on('click', function() {selectMapItem(false) });
+		map.on('dblclick', function() {
+			selectMapItem(false); 
+			$legend.removeClass('active'); 
+			$list.removeClass('active'); 
+			$filters.removeClass('active'); 
+		});
 
 		// MARKERS
 		for(var location of objMapData){
@@ -246,14 +179,49 @@ function initMap() {
 		map.addLayer(markersCluster);
 
 
-
+		// make sure we have some pertinent boundaries to rely on
 		if (!objMarkersBounds.hasOwnProperty('_southWest') || !objMarkersBounds.hasOwnProperty('_northEast')) 
 			objMarkersBounds = bounds;
-		if (objMapConfig.map.center)
-			map.setView(L.latLng({lat: parseFloat(objMapConfig.map.center.split(',')[0]), lng: parseFloat(objMapConfig.map.center.split(',')[1])}), objMapConfig.map.zoom);
-		else
-			map.setView(objMarkersBounds.getCenter(), objMapConfig.map.zoom);
-
+		
+		if (objMapConfig.map.fitBounds)
+			map.fitBounds(objMarkersBounds);
+		else{
+			if (objMapConfig.map.center)
+				map.setView(L.latLng({lat: parseFloat(objMapConfig.map.center.split(',')[0]), lng: parseFloat(objMapConfig.map.center.split(',')[1])}), objMapConfig.map.zoom);
+			else
+				map.setView(objMarkersBounds.getCenter(), objMapConfig.map.zoom);
+		}
 		resolve()
 	});
+}
+
+// override selectMapItem to add some spice on the map as we browse throught the list
+selectMapItem = function(itemID){
+	$list.removeClass('has-selected');
+	$map.find('.leaflet-marker-pane').removeClass('has-selected');
+	$list.find('.map__list__item').removeClass('selected');
+	$map.find('.leaflet-marker-pane .leaflet-marker-icon').removeClass('selected');
+	if (itemID) {
+		$list.addClass('has-selected');
+		$list.find('.map__list__item[data-id="'+itemID+'"]').addClass('selected').get(0).scrollIntoView({behavior: "smooth", block: 'center', inline: "nearest"});
+		var marker = arrMarkersAll.filter(function(marker){
+		    return marker.locationID == itemID
+		});
+		if (marker[0]._icon) {
+			$map.find('.leaflet-marker-pane').addClass('has-selected');
+			$(marker[0]._icon).addClass('selected')
+		} else{
+		 	markersCluster.zoomToShowLayer(marker[0],()=>{selectMapItem(itemID)})
+		}
+	}
+}
+markersCluster.on('clusterclick', function (a) {
+	selectMapItem(false);
+});
+
+applyFilters_callback = function(){
+	markersCluster.removeLayers(arrMarkersAll);
+	markersCluster.addLayers(arrMarkersCurrent);
+	if (objMapConfig.map.fitBounds)
+		map.fitBounds(markersCluster.getBounds());
 }
