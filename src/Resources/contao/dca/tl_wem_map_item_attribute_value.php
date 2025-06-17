469705ca060727583d77a2dee52897e6682a7b7b
LupusVII <jln.thirion@gmail.com>
2025-06-17 19:27:10 +0200

    Fix call

--- src/Resources/contao/dca/tl_wem_map_item_attribute_value.php
+++ src/Resources/contao/dca/tl_wem_map_item_attribute_value.php
@@ -10,18 +10,9 @@ declare(strict_types=1);
  * @license    https://www.apache.org/licenses/LICENSE-2.0
  */
 
+use Contao\DataContainer;
 use Contao\DC_Table;
 use WEM\GeoDataBundle\DataContainer\MapItemAttributeValue;
-
-/*
- * Geodata for Contao Open Source CMS
- * Copyright (c) 2015-2024 Web ex Machina
- *
- * @category ContaoBundle
- * @package  Web-Ex-Machina/contao-geodata
- * @author   Web ex Machina <contact@webexmachina.fr>
- * @link     https://github.com/Web-Ex-Machina/contao-geodata/
- */
 
 /*
  * Table tl_wem_map_item_attribute_value.
